023d251101ab9013b1585bfc067cb584fc3aec2e
antonio <antonio.alberto.f24@gmail.com>
2024-12-15 19:59:33 +0000

    login e signup trust the process

--- MovieHubFrontend/src/app/app.routes.ts
+++ MovieHubFrontend/src/app/app.routes.ts
@@ -5,6 +5,8 @@ import { ActorListComponent } from './components/actor-list/actor-list.component
 import { MovieListComponent } from './components/movie-list/movie-list.component';
 import { GenresListComponent } from './components/genres-list/genres-list.component';
 import { MovieDetailsComponent } from './components/movie-details/movie-details.component';
+import { LoginComponent } from './components/login/login.component';
+import { RegisterComponent } from './components/register/register.component';
 
 export const routes: Routes = [
 { path: '', component: HomeComponent },
@@ -13,5 +15,7 @@ export const routes: Routes = [
 { path: 'movies/:id', component: MovieDetailsComponent },
 { path: 'movies', component: MovieListComponent },
 { path: 'genres', component: GenresListComponent },
+{ path: 'login', component: LoginComponent},
+{ path: 'register', component: RegisterComponent}
 
 ];
