b2322cf0d0be7a6f927d9032ca3af6bd22b2a7cc
antonio <antonio.alberto.f24@gmail.com>
2024-12-15 22:19:35 +0000

    skill issue de virgula

--- MovieHubFrontend/src/app/app.routes.ts
+++ MovieHubFrontend/src/app/app.routes.ts
@@ -26,7 +26,7 @@ export const routes: Routes = [
     
 { path: 'genres', component: GenresListComponent },
 { path: 'login', component: LoginComponent},
-{ path: 'register', component: RegisterComponent}
+{ path: 'register', component: RegisterComponent},
 
 
 // Favorite
