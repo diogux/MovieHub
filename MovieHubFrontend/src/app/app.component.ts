e68557341e88a18571ce5abf6b073c9180e5b01e
diogux <diogomiguel.fernandes@ua.pt>
2024-12-16 01:50:00 +0000

    add emiter to app.component

--- MovieHubFrontend/src/app/app.component.ts
+++ MovieHubFrontend/src/app/app.component.ts
@@ -3,8 +3,9 @@ import { RouterModule, RouterOutlet } from '@angular/router';
 import { CommonModule } from '@angular/common';
 import { NavbarComponent } from "./components/navbar/navbar.component";
 import { FooterComponent } from "./components/footer/footer.component";
-// import { HttpClient } from '@angular/common/http';
-// import { Emitters } from './emitters/emitters'
+
+import { HttpClient } from '@angular/common/http';
+import { Emitters } from './emitters/emitters'
 
 @Component({
   selector: 'app-root',
@@ -13,8 +14,24 @@ import { FooterComponent } from "./components/footer/footer.component";
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'MovieHubFrontend';
+
+  constructor(
+    private http: HttpClient
+  ) { }
+
+  ngOnInit(): void {
+    this.http.get('http://localhost:8000/api/user', { withCredentials: true }).subscribe(
+      (res: any) => {
+        
+        Emitters.authEmitter.emit(true);
+      },
+      err => {
+        Emitters.authEmitter.emit(false);
+      }
+    );
+  }
 
 
 
