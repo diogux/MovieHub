a52c59a0048a955051d8866d79d48bb90ba3bc92
antonio <antonio.alberto.f24@gmail.com>
2024-12-16 00:33:18 +0000

    ja dei fix monstro

--- MovieHubFrontend/src/app/components/home/home.component.ts
+++ MovieHubFrontend/src/app/components/home/home.component.ts
@@ -19,7 +19,7 @@ export class HomeComponent implements OnInit {
   ) { }
 
   ngOnInit(): void {
-    this.http.get(environment.baseUrl + 'user').subscribe(
+    this.http.get(environment.baseUrl + 'user', { withCredentials: true }).subscribe(
       (res: any) => {
         
         Emitters.authEmitter.emit(true);
