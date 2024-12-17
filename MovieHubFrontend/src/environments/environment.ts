06bee17a60319f44a0d9b5213bfc40ab7a3cb8bd
diogux <diogomiguel.fernandes@ua.pt>
2024-12-17 23:46:22 +0000

    dasd

--- MovieHubFrontend/src/environments/environment.ts
+++ MovieHubFrontend/src/environments/environment.ts
@@ -1,6 +1,7 @@
 export const environment = {
   production: false,
-  baseUrl: 'http://localhost:8000/api/',
-  pictureUrl: 'http://localhost:8000/',
-  backendUrl: 'http://localhost:8000'
+  baseUrl: 'https://movieshubapp.pythonanywhere.com/api/',
+  pictureUrl: 'https://movieshubapp.pythonanywhere.com/',
+  backendUrl: 'https://movieshubapp.pythonanywhere.com'
 };
+
