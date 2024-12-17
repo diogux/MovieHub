a4c6fd7ae35d318784cfa1d85e74a8acc9c413bc
antonio <antonio.alberto.f24@gmail.com>
2024-12-17 14:19:07 +0000

    register perfect

--- MovieHubFrontend/src/app/components/register/register.component.ts
+++ MovieHubFrontend/src/app/components/register/register.component.ts
@@ -1,11 +1,9 @@
-import { Component , OnInit} from '@angular/core';
-import {ReactiveFormsModule} from '@angular/forms';
-import { FormBuilder, FormGroup } from '@angular/forms';
-import {HttpClient} from '@angular/common/http';
-import {Router} from '@angular/router';
+import { Component, OnInit } from '@angular/core';
+import { ReactiveFormsModule, Validators, FormBuilder, FormGroup } from '@angular/forms';
+import { HttpClient } from '@angular/common/http';
+import { Router, RouterModule} from '@angular/router';
 import { CommonModule } from '@angular/common';
 import { HttpClientModule } from '@angular/common/http';
-import { RouterModule } from '@angular/router';
 import { environment } from '../../../environments/environment';
 
 @Component({
@@ -13,30 +11,52 @@ import { environment } from '../../../environments/environment';
   standalone: true,
   imports: [CommonModule, HttpClientModule, RouterModule, ReactiveFormsModule],
   templateUrl: './register.component.html',
-  styleUrl: './register.component.css'
+  styleUrls: ['./register.component.css']
 })
 export class RegisterComponent implements OnInit {
   form: FormGroup = this.formBuilder.group({});
+  groupLabels = {
+    1: 'Movie Manager',
+    2: 'Crew Manager',
+    3: 'User'
+  };
+  errorMessage: string = ''; // Mensagem de erro
+
   constructor(
     private formBuilder: FormBuilder,
     private http: HttpClient,
     private router: Router
-  ) {
-
-  }
+  ) {}
 
   ngOnInit(): void {
     this.form = this.formBuilder.group({
-      username: '',
-      email: '',
-      password: '',
-      groups:[[3]]
+      username: ['', Validators.required],
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', Validators.required],
+      groups: [[], Validators.required]  
     });
   }
 
-  submit(): void{
-    console.log(this.form.getRawValue());
-    this.http.post(environment.baseUrl + 'register', this.form.getRawValue())
-    .subscribe( () => this.router.navigate(['/login']));
+  submit(): void {
+    const formData = this.form.getRawValue(); 
+
+    const { groups, ...restOfFormData } = formData;
+
+    const groupsArray = Array.isArray(groups) ? groups.map(group => parseInt(group, 10)) : [parseInt(groups, 10)];
+
+    if (groupsArray.length < 1) {
+      return; 
+    }
+
+    const finalData = { ...restOfFormData, groups: groupsArray };
+
+    console.log('Dados enviados:', finalData);
+
+    // Enviar os dados para a API
+    this.http.post(environment.baseUrl + 'register', finalData)
+      .subscribe({
+        next: () => this.router.navigate(['/login']),
+        error: (err) => console.error('Erro ao enviar dados:', err)
+      });
   }
 }
